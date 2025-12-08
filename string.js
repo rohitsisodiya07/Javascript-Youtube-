10dbc02251d8c904bccf3b77b2f8b9fa95f3632c
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2025-12-08 20:39:01 +0530

    commit done

--- string.js
+++ string.js
@@ -1,4 +1,4 @@
-let name = "Rohit" ;
+let name = "Rohit";
 // let name = 'Rohit' ; // String Also Assign With Single Quote
 console.log(name.length); // For Getting Length of String
 
@@ -8,7 +8,7 @@ console.log(name[1]); // For Accessing Chracter For String It Start with Index(0
 //Template Literals
 let myName = "Rohit Sisodiya" ;
 let myAge = 21 ;
-let sentence = `My Name is ${myName} and i am ${myAge} years old .` ; //Template Literals Use Backticksinstead of Quote to Define a String
+let sentence = `My Name is ${myName} and i am ${myAge} years old .` ; //Template Literals Use Backticks instead of Quote to Define a String
 console.log(sentence);
  
 //Escape Sequence Character
