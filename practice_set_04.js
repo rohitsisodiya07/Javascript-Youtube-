7454ad8eaf84b0f368c6ff05a5fdddc447a4c59d
Rohit Sisodiya <rohitsisodiya2503@gmail.com>
2025-12-08 15:52:35 +0530

    Commit Done

---
+++ practice_set_04.js
@@ -0,0 +1,17 @@
+//Q.1 
+    // let a = "har\""
+    // console.log(a.length);
+    
+//Q.2
+    // let name = "ROHIT"
+    // console.log(name.toLowerCase());
+    
+//Q.3
+    let amount = "Please give Rs1000"
+    let number = Number.parseInt(amount.slice(14));
+    console.log(number);
+    
+    
+//Q.4
+    // let change = "Change"
+    // change[2] = 'o' //Not Possible Because Strings are Immutable
---
+++ strings_methods.js
@@ -0,0 +1,31 @@
+let name = "Rohit"
+let another = "Rohit Bhai"
+
+
+//Strings are Immutable we Cannot Change it.
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);//For Access String Character
+name[1] = 'o' //This is not Possible
+
+
+
+// console.log(name.length); // Property of String
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+// console.log(name.slice(0,3)); // Print Index From 0 to 3, But 3 not include
+// console.log(name.slice(2)); // Print Index From 3 to end
+
+// console.log(another.replace("Bhai", "Bhau"));
+// console.log(another.replace('h', "a")); // Only Replace first Occurence Character 
+// console.log(another.replaceAll('h', "a")); // Replace all Occurence
+
+
+// console.log(name.concat(another));// Adds Two String
+
+// console.log(name);
+// console.log(name.trim()); // Remove White Space
+
+
